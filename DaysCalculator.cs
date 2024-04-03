d8b28f02a87e4043e094d0383ef10a3068ce067f
Thanay Kumar Y R <thanaykumaryr@gmail.com>
2024-04-03 15:34:22 +0530

    added Regex validation and exception handing

--- DaysCalculator.cs
+++ DaysCalculator.cs
@@ -11,30 +11,45 @@ namespace HotelReservation
     {
         public (int, int) Count()
         {
-            int weekEndCount = 0, weekDayCount = 0;
-            string format = "ddMMMyyyy";
-            CultureInfo provider = CultureInfo.InvariantCulture;
-            Console.WriteLine("Reservation Dates");
-            Console.Write("Enter the strating date in ddMMMyyyy format = ");
-            DateTime start = DateTime.ParseExact(Console.ReadLine(), format, provider);
-            Console.Write("Enter the ending date in ddMMMyyyy format = ");
-            DateTime end = DateTime.ParseExact(Console.ReadLine(), format, provider);
-            if (start > end)
+            try
             {
+                Validation validation = new Validation();
+
+                int weekEndCount = 0, weekDayCount = 0;
+                string format = "ddMMMyyyy";
+                CultureInfo provider = CultureInfo.InvariantCulture;
+                Console.WriteLine("Reservation Dates");
+                Console.Write("Enter the strating date in ddMmmyyyy format = ");
+                string strStart = Console.ReadLine();
+                if (!validation.Check(strStart)) throw new ApplicationException("Start date is not in correct format !");
+                DateTime start = DateTime.ParseExact(strStart, format, provider);
+                Console.Write("Enter the ending date in ddMmmyyyy format = ");
+                string strEnd = Console.ReadLine();
+                if (!validation.Check(strStart)) throw new ApplicationException("End date is not in correct format !");
+                DateTime end = DateTime.ParseExact(strEnd, format, provider);
+                if (start > end)
+                {
+                    return (-1, -1);
+                }
+                for (DateTime date = start; date <= end; date = date.AddDays(1))
+                {
+                    if (date.DayOfWeek == DayOfWeek.Sunday || date.DayOfWeek == DayOfWeek.Saturday)
+                    {
+                        weekEndCount++;
+                    }
+                    else
+                    {
+                        weekDayCount++;
+                    }
+                }
+                return (weekDayCount, weekEndCount);
+            }
+            catch(Exception ex) 
+            {
+                Console.WriteLine(ex.Message);
                 return (-1, -1);
             }
-            for (DateTime date = start; date <= end; date = date.AddDays(1))
-            {
-                if (date.DayOfWeek == DayOfWeek.Sunday || date.DayOfWeek == DayOfWeek.Saturday)
-                {
-                    weekEndCount++;
-                }
-                else
-                {
-                    weekDayCount++;
-                }
-            }
-            return (weekDayCount, weekEndCount);
+            
         }
     }
 }
