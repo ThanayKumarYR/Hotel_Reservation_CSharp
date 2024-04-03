1e515349d2c14594989c84ddbc0a14522448d402
Thanay Kumar Y R <thanaykumaryr@gmail.com>
2024-04-03 16:07:59 +0530

    fixing bug

---
+++ DaysCalculator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservation
+{
+    class DaysCalculator
+    {
+        public (int, int) Count()
+        {
+            try
+            {
+                Validation validation = new Validation();
+
+                int weekEndCount = 0, weekDayCount = 0;
+                string format = "ddMMMyyyy";
+                CultureInfo provider = CultureInfo.InvariantCulture;
+                Console.WriteLine("Reservation Dates");
+                Console.Write("Enter the strating date in ddMmmyyyy format = ");
+                string strStart = Console.ReadLine();
+                if (!validation.Check(strStart)) throw new ApplicationException("Start date is not in correct format !");
+                DateTime start = DateTime.ParseExact(strStart, format, provider);
+                Console.Write("Enter the ending date in ddMmmyyyy format = ");
+                string strEnd = Console.ReadLine();
+                if (!validation.Check(strEnd)) throw new ApplicationException("End date is not in correct format !");
+                DateTime end = DateTime.ParseExact(strEnd, format, provider);
+                if (start > end)
+                {
+                    return (-1, -1);
+                }
+                for (DateTime date = start; date <= end; date = date.AddDays(1))
+                {
+                    if (date.DayOfWeek == DayOfWeek.Sunday || date.DayOfWeek == DayOfWeek.Saturday)
+                    {
+                        weekEndCount++;
+                    }
+                    else
+                    {
+                        weekDayCount++;
+                    }
+                }
+                return (weekDayCount, weekEndCount);
+            }
+            catch(Exception ex) 
+            {
+                Console.WriteLine(ex.Message);
+                return (-1, -1);
+            }
+            
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -11,6 +11,80 @@ namespace HotelReservation
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Hotel Reservation !");
+
+            Hotel[] hotels = new Hotel[3];
+            DaysCalculator daysCalculator = new DaysCalculator();
+
+            hotels[0] = new Hotel("Lakewood",110,90,3,80,80);
+            hotels[1] = new Hotel("Bridgewood", 150,50,4,110,50);
+            hotels[2] = new Hotel("Ridgewood", 220,150,5,100,40);
+
+            Console.WriteLine("\n1.Cheapest Hotels 2.BestRated Hotels 3.Exit");
+            Console.Write("Enter the choice = ");
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Finding the Cheapest hotel !");
+                    var (CheapweekDays, CheapWeekEnds) = daysCalculator.Count();
+                    if (CheapweekDays != -1 && CheapWeekEnds != -1)
+                    {
+                        Console.WriteLine("1.Regular Customer 2.Reward Customer");
+                        int choosing = int.Parse(Console.ReadLine());
+                        switch (choosing)
+                        {
+                            case 1:
+                                Console.WriteLine("Finding the Cheapest hotel for Regular Customer");
+                                Hotel.CheapestRegular(hotels, CheapweekDays, CheapWeekEnds);
+                                break;
+                            case 2:
+                                Console.WriteLine("Finding the Cheapest hotel for Reward Customer");
+                                Hotel.CheapestRewards(hotels, CheapweekDays, CheapWeekEnds);
+                                break;
+                            default:
+                                Console.WriteLine("Invalid Inputs !");
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid Dates");
+                    }
+                    break;
+                case 2:
+                    Console.WriteLine("Finding the Best Hotels !");
+                    var (BestweekDays, BestWeekEnds) = daysCalculator.Count();
+                    if (BestweekDays != -1 && BestWeekEnds != -1)
+                    {
+                        Console.WriteLine("1.Regular Customer 2.Reward Customer");
+                        int choosing = int.Parse(Console.ReadLine());
+                        switch (choosing)
+                        {
+                            case 1:
+                                Console.WriteLine("Finding the Best hotel for Regular Customer");
+                                Hotel.BestRegular(hotels, BestweekDays, BestWeekEnds);
+                                break;
+                            case 2:
+                                Console.WriteLine("Finding the Best hotel for Reward Customer");
+                                Hotel.BestRewards(hotels, BestweekDays, BestWeekEnds);
+                                break;
+                            default:
+                                Console.WriteLine("Invalid Inputs !");
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid Dates");
+                    }
+                    break;
+                case 3:
+                    return;
+                default:
+                    Console.WriteLine("Invalid choice");
+                    break;
+            }
         }
     }
 }
